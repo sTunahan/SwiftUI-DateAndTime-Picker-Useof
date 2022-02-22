9e0ec61440d890f57de00d0c852ead0344051531
sTunahan <st.ustuntepe@gmail.com>
2022-02-22 10:13:45 +0300

    Initial Commit

---
+++ Data and Time Picker Useof/Data_and_Time_Picker_UseofApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_and_Time_Picker_UseofApp.swift
+//  Data and Time Picker Useof
+//
+//  Created by asuftech on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Data_and_Time_Picker_UseofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
